52c83a7be265bd4d6690f1608235b48c3459151f
Chrisnatalx <natale.christian.a@gmail.com>
2024-02-13 11:33:21 -0300

    Add bg-gradient in body and add color in tailwind config

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,27 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
-import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
+const inter = Inter({ subsets: ['latin'] });
+import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 
 export const metadata = {
-  title: "Mr.Billy bob",
-  description: "las mejores hamburgesas",
+	title: 'Mr.Billy bob',
+	description: 'las mejores hamburgesas',
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-      }}
-    >
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider
+			appearance={{
+				baseTheme: dark,
+			}}
+		>
+			<html lang="en">
+				<body className="bg-gradient-to-tr from-slate-800 to-regal-blue">
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
