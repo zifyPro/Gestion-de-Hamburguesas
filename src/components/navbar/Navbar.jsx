b10de68eb895db998ed700c90dbf67a3d697401b
Diaz027 <dami27cito@gmail.com>
2024-02-13 17:26:39 -0300

    boton home

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
-import { GiTrophy } from 'react-icons/gi';
-import Users from '../Users/Users';
+import React from "react";
+import { GiTrophy } from "react-icons/gi";
+import Users from "../Users/Users";
+import Link from "next/link";
 const Navbar = () => {
-	return (
-		<>
-			<div className="flex flex- justify-around items-center pt-6">
-				<Users />
-				<button className=" text-white scale-150 flex flex-row">
-					<a className="flex flex-row items-center font-sans font-semibold italic text-xs text-nowrap my-auto mx-2 ">
-						<GiTrophy className="w-10 h-5 text-white " />
-						700
-					</a>
-				</button>
-			</div>
-			<div className="flex justify-center items-center ">
-				<img className="w-1/3 md:w-1/6" src="./logo.png" alt="logo billiebob" />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="flex flex- justify-around items-center pt-6">
+        <Users />
+        <button className=" text-white scale-150 flex flex-row">
+          <a className="flex flex-row items-center font-sans font-semibold italic text-xs text-nowrap my-auto mx-2 ">
+            <GiTrophy className="w-10 h-5 text-white " />
+            700
+          </a>
+        </button>
+      </div>
+      <div className="flex justify-center items-center ">
+        <Link href="/">
+          <img
+            className="w-1/3 md:w-1/6"
+            src="/logo.png"
+            alt="logo billiebob"
+          />
+        </Link>
+      </div>
+    </>
+  );
 };
 export default Navbar;
