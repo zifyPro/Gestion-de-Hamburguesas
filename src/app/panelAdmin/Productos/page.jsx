ad2fa0156b40bc8fec6092b8f0f20a0fc01e75fb
Diaz027 <dami27cito@gmail.com>
2024-02-15 16:38:11 -0300

    panelAdmin/Productos

---
+++ src/app/panelAdmin/Productos/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+const Productos = () => {
+  return (
+    <div>
+      <h1>aca estan los productos</h1>
+    </div>
+  );
+};
+export default Productos;
--- src/app/panelAdmin/page.jsx
+++ src/app/panelAdmin/page.jsx
@@ -1,11 +1,15 @@
 const PanelAdmin = () => {
   return (
-    <div className="flex flex-center justify-around items-center p-6">
-      <h1>esto es el panel administrador</h1>
-
-      <button>
-        <a href="/panelAdmin/Usuarios">users</a>
-      </button>
+    <div className="flex flex-col items-center">
+      <h1 className="mb-4">esto es el panel administrador</h1>
+      <div className="flex justify-around w-full">
+        <button className="flex justify-center  items-center p-6">
+          <a href="/panelAdmin/Productos">Productos</a>
+        </button>
+        <button className="flex justify-center items-center p-2">
+          <a href="/panelAdmin/Usuarios">users</a>
+        </button>
+      </div>
     </div>
   );
 };
