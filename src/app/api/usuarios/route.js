0d62c2378b894867bfdde8fb42f03addc8951b3d
Diaz027 <dami27cito@gmail.com>
2024-02-15 11:58:59 -0300

    Users.created/Clerk

--- src/app/api/usuarios/route.js
+++ src/app/api/usuarios/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
 
 export async function GET() {
-  // const newUsers = await prisma.user.findMany();
-  // console.log(newUsers);
-  return NextResponse.json("holaaaaaa");
+  const newUsers = await prisma.user.findMany();
+  console.log(newUsers);
+  return NextResponse.json(newUsers);
 }
