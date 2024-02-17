d79346541981b2e6256e975f822edc2591238110
Chrisnatalx <natale.christian.a@gmail.com>
2024-02-17 14:12:34 -0300

    Delete space in store

--- src/components/burgers/Burger.jsx
+++ src/components/burgers/Burger.jsx
@@ -9,23 +9,23 @@
 //   );
 // };
 // export default Burgers;
-"use client";
-import { useEffect } from "react";
-import useStore from "../../zustand/store/index";
-import Cards from "../cards/Cards";
+'use client';
+import { useEffect } from 'react';
+import useStore from '../../zustand/store/index';
+import Cards from '../cards/Cards';
 
 const Burgers = () => {
-  const product = useStore((state) => state.product);
-  const getHamburguesas = useStore((state) => state.getHamburguesas);
+	const product = useStore((state) => state.product);
+	const getHamburguesas = useStore((state) => state.getHamburguesas);
 
-  useEffect(() => {
-    getHamburguesas();
-  }, []);
+	useEffect(() => {
+		getHamburguesas();
+	}, []);
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Cards product={product} />
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center ">
+			<Cards product={product} />
+		</div>
+	);
 };
 export default Burgers;
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,27 +1,30 @@
 const Card = (product) => {
-  return (
-
-
-
-<div class="mx-auto bg-custom-gray rounded-xl shadow-md overflow-hidden w-11/12 m-5 h-32">
-  <div class="flex">
-    <div class="flex-shrink-0">
-      <img class="h-32 w-3/5" src={product?.img} alt="Imagen de comida"/>
-    </div>
-    <div class="-ml-20">
-      <div class="uppercase tracking-wide text-sm text-gray-text font-semibold w-32 relative  text-lg " >{product?.title}</div>
-      <p class="mt-2 text-gray-500  ">{product?.description}</p>
-      <a href="#" class="block text-lg leading-tight font-medium text-yellow-500 hover:underline -mt-1">Leer más</a>
-    </div>
-    <div class="p-2 mt-7">
-      <div class="text-lg font-bold text-white">{"$"+product?.price}</div>
-      <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4">
-        Ordenar
-      </button>
-    </div>
-  </div>
-</div>
-
-  );
+	return (
+		<div class="mx-auto bg-custom-gray rounded-xl shadow-md overflow-hidden w-11/12 m-5 h-32">
+			<div class="flex ">
+				<div class="flex-shrink-0">
+					<img class="h-32 w-3/5" src={product?.img} alt="Imagen de comida" />
+				</div>
+				<div class="-ml-20">
+					<div class="uppercase tracking-wide  text-gray-text font-semibold w-32 relative  text-lg ">
+						{product?.title}
+					</div>
+					<p class="mt-2 text-gray-500  ">{product?.description}</p>
+					<a
+						href="#"
+						class="block text-lg leading-tight font-medium text-yellow-500 hover:underline -mt-1"
+					>
+						Leer más
+					</a>
+				</div>
+				<div class="p-2 mt-7">
+					<div class="text-lg font-bold text-white">{'$' + product?.price}</div>
+					<button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4">
+						Ordenar
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 export default Card;
