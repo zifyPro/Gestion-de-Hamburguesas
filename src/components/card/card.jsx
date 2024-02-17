1db7933ec73bf4807f117179a1b1308dcd6c0f05
Gabriel J. <106175930+zuoki@users.noreply.github.com>
2024-02-17 13:35:27 -0300

    front 0.0.6

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,28 +1,27 @@
 const Card = (product) => {
   return (
-    // <div className=" list-item items-center ">
-    <div className=" mx-auto  text-gray-text font-semibold w-10/12 md:w-2/4 ">
-      <div className=" bg-custom-gray w-10/12 md:w-2/3   mx-auto mt-5 rounded-lg">
-        <h1 className="w-12 mx-auto  text-gray-text font-semibold">
-          {product?.title}
-        </h1>
-        <img className="w-3/4 mx-auto rounded-lg" src={product?.img} />
-        <h2 className="text-gray-500 font-sans font-semibold italic text-left ml-4 sm:text-xl m-4 text-xs">
-          {product?.description}
-        </h2>
 
-        <div className="flex justify-between">
-          <h1 className="  text-gray-text font-semibold ml-12 mt-2">
-            {"$" + product?.price}{" "}
-          </h1>
-          <button className=" bg-black rounded-md  text-yellow-400 font-semibold mr-12 mb-2 mt-2">
-            {" "}
-            agregar al carrito
-          </button>
-        </div>
-      </div>
+
+
+<div class="mx-auto bg-custom-gray rounded-xl shadow-md overflow-hidden w-11/12 m-5 h-32">
+  <div class="flex">
+    <div class="flex-shrink-0">
+      <img class="h-32 w-3/5" src={product?.img} alt="Imagen de comida"/>
     </div>
-    // </div>
+    <div class="-ml-20">
+      <div class="uppercase tracking-wide text-sm text-gray-text font-semibold w-32 relative  text-lg " >{product?.title}</div>
+      <p class="mt-2 text-gray-500  ">{product?.description}</p>
+      <a href="#" class="block text-lg leading-tight font-medium text-yellow-500 hover:underline -mt-1">Leer más</a>
+    </div>
+    <div class="p-2 mt-7">
+      <div class="text-lg font-bold text-white">{"$"+product?.price}</div>
+      <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4">
+        Ordenar
+      </button>
+    </div>
+  </div>
+</div>
+
   );
 };
 export default Card;
