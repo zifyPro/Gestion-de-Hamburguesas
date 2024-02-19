65fcca60abbb1ebff9ee189f513e8bacbb63d083
Gabriel J. <106175930+zuoki@users.noreply.github.com>
2024-02-19 16:53:55 -0300

    code 0.2.6

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -36,18 +36,18 @@ const Card = (product) => {
 
 
 
-<div className="mx-auto bg-black rounded-xl shadow-md overflow-hidden w-[92vw] m-4 h-32 md:w-2/3">
+<div className="mx-auto bg-black rounded-xl shadow-md overflow-hidden w-[92vw] m-4 h-32 md:w-11/12">
   <div className="flex">
     <div className="flex-shrink-0">
       <img className="h-32 w-3/5 object-cover" src={product?.img} alt="Imagen de comida"/>
     </div>
     <div className="-ml-20 w-35">
-      <div className="uppercase tracking-wide text-gray-text font-semibold w-32 relative  text-lg " >{product?.title}</div>
+      <div className="uppercase tracking-wide text-gray-text font-semibold w-32 relative  text-lg text-nowrap	" >{product?.title}</div>
       <p className="mt-2 text-gray-500  h-13  line-clamp-3 w-6/12 md:w-4/12">{product?.description}</p>
       <button className="block text-lg leading-tight font-medium text-yellow-500 hover:underline -mt-1" onClick={alertDetails}>Leer más</button>
     </div>
     <div className="mt-10 -ml-[15vw] ">
-      <div className="text-lg font-bold text-white -mt-[4vh] -ml-[7vh]  md:-[6.7vh]">{"$"+product?.price}</div>
+      <div className="text-lg font-bold text-white sm:-mt-[6vh] -mt-[4.5vh] -ml-[7vh]  md:-mt-[3vh]">{"$"+product?.price}</div>
       <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-[6.7vh] md:mt-10 -ml-[7vh]" onClick={alertAdd}>
       <FaCartPlus />
       </button>
