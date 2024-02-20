9f0b6b4c5eb880d166082ade7fa4d75eef72dd21
Diaz027 <dami27cito@gmail.com>
2024-02-20 11:38:18 -0300

    cards

--- src/app/panelAdmin/Productos/page.jsx
+++ src/app/panelAdmin/Productos/page.jsx
@@ -1,23 +1,31 @@
 "use client";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import useStore from "@/zustand/store";
 import Link from "next/link";
 import Swal from "sweetalert2";
+import Cards from "@/components/cards/Cards";
 
 const Productos = () => {
   const [data, setData] = useState([]);
+  const { getHamburguesas, setFilter, product } = useStore((state) => ({
+    getHamburguesas: state.getHamburguesas,
+    setFilter: state.setFilter,
+    product: state.product,
+  }));
 
   useEffect(() => {
-    const fetch = async () => {
-      const API_URL =
-        process.env.NODE_ENV === "development"
-          ? process.env.NEXT_PUBLIC_URL_REQUESTS_PRODUCTOS_LOCAL
-          : process.env.NEXT_PUBLIC__PROD_URL_REQUESTS_PRODUCTOS_DEPLOY;
-      const response = await axios.get(API_URL);
-      const result = response.data;
-      setData(result);
-    };
-    fetch();
+    setData(getHamburguesas());
+    // const fetch = async () => {
+    //   const API_URL =
+    //     process.env.NODE_ENV === "development"
+    //       ? process.env.NEXT_PUBLIC_URL_REQUESTS_PRODUCTOS_LOCAL
+    //       : process.env.NEXT_PUBLIC__PROD_URL_REQUESTS_PRODUCTOS_DEPLOY;
+    //   const response = await axios.get(API_URL);
+    //   const result = response.data;
+    //   setData(result);
+    // };
+    // fetch();
   }, []);
 
   const deleteProduct = async (id) => {
@@ -35,12 +43,36 @@ const Productos = () => {
     return result;
   };
 
+  const newFilter = product.filter(
+    (p) => filter === "all" || p.type === filter
+  );
+
   return (
     <div className="list-item items-center ">
       <Link href="/panelAdmin/Productos/create">
         <button>Crear productos</button>
       </Link>
-      {data.map((element) => {
+      <button
+        onClick={() => {
+          setFilter("comida");
+        }}
+        className="bg-slate-800 hover:bg-slate-700  text-white rounded m-3 w-20 shadow-md md:w-24 md:m-20 h-10 font-sans font-semibold italic "
+      >
+        Burgers
+      </button>
+      <button
+        onClick={() => setFilter("bebida")}
+        className="bg-slate-800 hover:bg-slate-700 text-white rounded m-3 w-20 shadow-md md:w-24 md:m-20 h-10 font-sans font-semibold italic "
+      >
+        Bebidas
+      </button>
+      <button
+        onClick={() => setFilter("all")}
+        className="bg-slate-800 hover:bg-slate-700 text-white rounded m-3 w-20 shadow-md md:w-24 md:m-20 h-10 font-sans font-semibold italic "
+      >
+        todos
+      </button>
+      {newFilter.map((element) => {
         return (
           <div
             key={element.id}
@@ -69,6 +101,9 @@ const Productos = () => {
           </div>
         );
       })}
+      {/* <div>
+        <Cards product={product} />
+      </div> */}
     </div>
   );
 };
--- src/zustand/store/index.js
+++ src/zustand/store/index.js
@@ -5,7 +5,11 @@ const useStore = create((set) => ({
   product: [],
   filter: "all",
   getHamburguesas: async () => {
-    const response = await axios.get(`api/Productos`);
+    const API_URL =
+      process.env.NODE_ENV === "development"
+        ? process.env.NEXT_PUBLIC_URL_REQUESTS_PRODUCTOS_LOCAL
+        : process.env.NEXT_PUBLIC__PROD_URL_REQUESTS_PRODUCTOS_DEPLOY;
+    const response = await axios.get(API_URL);
     set({ product: response.data });
   },
   setFilter: (filter) => set({ filter }), // nueva función para cambiar el filtro
