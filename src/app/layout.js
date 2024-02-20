912d4504659a1c7b19445de455bdcea5cc85367e
Gabriel J. <106175930+zuoki@users.noreply.github.com>
2024-02-20 18:01:49 -0300

    code 0.2.9

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
         baseTheme: dark,
       }}
     >
-      <html lang="en">
+      <html lang="es">
         <body className="bg-[url('https://res.cloudinary.com/divxrmzge/image/upload/v1708176856/ojmk9i0nuuov2f2ahtke.jpg')] bg-cover bg-center">
           <Navbar />
           {children}
