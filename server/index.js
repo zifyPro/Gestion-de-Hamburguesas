d64e24a9b0909038e245d979a893f94b2f99e3b7
Diaz027 <dami27cito@gmail.com>
2024-02-21 17:58:39 -0300

    mp

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-import express from "express";
+import express, { request } from "express";
 import cors from "cors";
 
 //SDK de MercadoPago
 import { MercadoPagoConfig, Preference } from "mercadopago";
 const client = new MercadoPagoConfig({
-  accessToken:"TEST-4626481007888795-021216-556c3f583911cdf3d4e0d751386d040d-744460388",
+  accessToken: `TEST-4626481007888795-021216-556c3f583911cdf3d4e0d751386d040d-744460388`,
 });
 
 const app = express();
@@ -19,24 +19,33 @@ app.get("/", (req, res) => {
 
 app.post("/create_preference", async (req, res) => {
   try {
-    const body = {
-      items: [
-        {
-          title: req.body.title,
-          quantity: Number(req.body.quantity),
-          unit_price: Number(req.body.price),
-          currency_id: "ARS",
-        },
-      ],
-      back_urls: {
-        success: "http://youtube.com",
-        failure: "http://youtube.com",
-        pending: "http://youtube.com",
-      },
-      auto_return: "approved",
-    };
+    const data = req.body.carrito.map((element) => {
+      return {
+        title: element?.title,
+        quantity: element?.quantity,
+        unit_price: element.price,
+      };
+    });
+    // items: [
+    //   {
+    //     title: req.body.title,
+    //     quantity: req.body.quantity,
+    //     unit_price: Number(req.body.price),
+    //     currency_id: "ARS",
+    //   },
+    // ],
+    // back_urls: {
+    //   success: "http://youtube.com",
+    //   failure: "http://youtube.com",
+    //   pending: "http://youtube.com",
+    // },
+    // auto_return: "approved",
+    // };
+    console.log(data);
+    console.log("nnnnnnnnn", req.body);
     const preference = new Preference(client);
-    const result = await preference.create({ body });
+    const result = await preference.create({ element: data });
+    console.log(result);
     res.json({
       id: result.id,
     });
--- src/components/mercadoPago/mercadoPago.jsx
+++ src/components/mercadoPago/mercadoPago.jsx
@@ -1,13 +1,17 @@
 "use client";
 import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
 import { useState } from "react";
+import useStore from "@/zustand/store";
 import axios from "axios";
 
-const Products = (productToBuy) => {
+const Products = () => {
   const [preferenceId, setPreferenceId] = useState(null);
   initMercadoPago(process.env.NEXT_PUBLIC_KEY, {
     locale: "es-AR",
   });
+  const { cart } = useStore((state) => ({
+    cart: state.cart,
+  }));
 
   const API_URL_MERCADO_PAGO =
     process.env.NODE_ENV === "development"
@@ -15,11 +19,14 @@ const Products = (productToBuy) => {
       : process.env.NEXT_PUBLIC_URL_REQUESTS_MERCADO_PAGO_DEPLOY;
   const createPreference = async () => {
     try {
-      const response = await axios.post(API_URL_MERCADO_PAGO, {
-        titlle: "hamburgesa",
-        quantity: 1,
-        price: 100,
-      });
+      const carrito = cart.map((product) => ({
+        title: product.title,
+        quantity: 2,
+        unit_price: product.price,
+      }));
+      console.log("111111111111111111", carrito);
+      const response = await axios.post(API_URL_MERCADO_PAGO, carrito);
+      console.log("aaaaaaaaaaaaaaaa", response.data);
       const { id } = response.data;
       return id;
     } catch (error) {
@@ -35,10 +42,7 @@ const Products = (productToBuy) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h2>Hamburguesas</h2>
-      <img src="/descarga.jpeg" />
-      <p className="text-center">$2000</p>
+    <div className=" w-[95vh] md:w-2/3 mx-auto bg-custom-gray">
       <button onClick={handleBuy}>comprar</button>
       {preferenceId && (
         <Wallet
