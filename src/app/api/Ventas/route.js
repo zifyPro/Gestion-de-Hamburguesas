4652dfd219448ce017b8cc85ba99e7d8b7840ffd
Diaz027 <dami27cito@gmail.com>
2024-02-21 20:20:31 -0300

    price total

--- src/app/api/Ventas/route.js
+++ src/app/api/Ventas/route.js
@@ -1,38 +1,30 @@
-import { NextResponse } from 'next/server';
-import { prisma } from '@/libs/prisma';
+import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
 export async function GET() {
-	const ventas = await prisma.ventas.findMany({
-		include: {
-			productos: true,
-		},
-	});
-
-	ventas.forEach((venta) => {
-		let precioTotal = 0;
-		venta.productos.forEach((producto) => {
-			precioTotal += producto.price;
-		});
-		venta.precioTotal = precioTotal;
-	});
-	return NextResponse.json(ventas);
+  const ventas = await prisma.ventas.findMany({
+    include: {
+      productos: true,
+    },
+  });
+  return NextResponse.json(ventas);
 }
 
 export async function POST(request) {
-	const { nombre, telefono, direccion, numeroDeOrden, titleProductos } =
-		await request.json();
-	const newVenta = await prisma.ventas.create({
-		data: {
-			nombre,
-			telefono,
-			direccion,
-			numeroDeOrden,
-			productos: {
-				connect: {
-					title: titleProductos,
-				},
-			},
-		},
-	});
-	return NextResponse.json(newVenta);
+  const { nombre, telefono, direccion, numeroDeOrden, titleProductos } =
+    await request.json();
+  const newVenta = await prisma.ventas.create({
+    data: {
+      nombre,
+      telefono,
+      direccion,
+      numeroDeOrden,
+      productos: {
+        connect: {
+          title: titleProductos,
+        },
+      },
+    },
+  });
+  return NextResponse.json(newVenta);
 }
