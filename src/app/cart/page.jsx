149f75d6a7408b37471f73bf74c9a1d4ec805ff7
Diaz027 <dami27cito@gmail.com>
2024-02-23 12:58:13 -0300

    a

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import MercadoPago from "@/components/mercadoPago/mercadoPago";
 import useStore from "@/zustand/store";
+import { MdDeleteForever } from "react-icons/md";
 
 const Cart = () => {
   const { cart, deleateProductToCart } = useStore((state) => ({
@@ -10,16 +11,20 @@ const Cart = () => {
 
   return (
     <>
-      <div className=" w-[95vh] md:w-2/3 mx-auto bg-custom-gray">
+      <div className=" flex justify-center flex-col items-center">
         {cart.map((product) => {
           return (
             <div key={product.id} className="bg-gray-600 w-3/4">
               <h2>{product.title}</h2>
-              <img src={product.img} />
-              <p>{product.price}</p>
-              <button onClick={() => deleateProductToCart(product.id)}>
-                {" "}
-                eliminar
+              <img className="h-22 w-2/4 object-cover" src={product.img} />
+              <p className="flex justify-center -ml-[-32vh]">
+                {"$" + product?.price}
+              </p>
+              <button
+                className="flex justify-center bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-6 rounded mt-[6.7vh] md:mt-10 -ml-[-7vh]"
+                onClick={() => deleteProduct(product?.id)}
+              >
+                <MdDeleteForever />
               </button>
             </div>
           );
